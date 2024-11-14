a9d20bd0b5a3110067150f6b58a9cc10007a04c2
May Sunktong <pattaranan.sunktong@gmail.com>
2024-11-14 14:07:15 +0100

    ✅ 6/6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  const isYearDividedBy4 = year % 4 === 0;
+  const isYearDividedBy100 = year % 100 === 0;
+  const isYearDividedBy400 = year % 400 === 0;
+  // no need to write true:false
+  return isYearDividedBy4 && (!isYearDividedBy100 || isYearDividedBy400);
 };
 
 // Do not edit below this line
